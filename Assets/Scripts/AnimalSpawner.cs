b93247eeb22653d00d9d49ec4aa331bb5638986f
explosiveseccc <rdericieux25@aoiths.org>
2023-10-06 12:56:56 -0400

    2.3 done

---
+++ Assets/Scripts/AnimalSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalSpawner : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    float SpawnRangeX = 20;
+    float spawnPosZ = 20;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // this makes it so when you press s it randomly spawns a animal 
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-SpawnRangeX, SpawnRangeX), 0, spawnPosZ); 
+            
+            int animalIndex = Random.Range(0, animalPrefabs.Length); 
+            
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+        }
+    }
+}
